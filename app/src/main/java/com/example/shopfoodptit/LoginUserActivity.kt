b10d0d3665ddb6605d8db2b198f8ab0dfe911321
ToBi <lehoaiduybinh.pch.2b7.97@gmail.com>
2024-03-05 08:46:12 +0700

    update login and sign up user

--- app/src/main/java/com/example/shopfoodptit/LoginUserActivity.kt
+++ app/src/main/java/com/example/shopfoodptit/LoginUserActivity.kt
@@ -1,15 +1,63 @@
 package com.example.shopfoodptit
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.telecom.Call
+import android.util.Log
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import com.example.shopfoodptit.model.UserModel
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.FacebookSdk
+import com.facebook.appevents.AppEventsLogger
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.tasks.Task
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.auth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.database
+import java.util.Arrays
 
 class LoginUserActivity : AppCompatActivity() {
+
+    private lateinit var email: String
+    private lateinit var password: String
+    private var userName: String ?= null
+    private lateinit var auth: FirebaseAuth
+    private lateinit var database: DatabaseReference
+    private lateinit var edtPassword: EditText
+    private lateinit var edtEmail: EditText
+    private lateinit var googleSignInclient: GoogleSignInClient
+    private var callbackManager: CallbackManager?= null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_user)
+        setUp()
+        val googleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id)).requestEmail().build()
+        // Kết nối firebase để login
+        auth = Firebase.auth
+        database = Firebase.database.reference
+
+        // Kết nối login bằng google
+        googleSignInclient = GoogleSignIn.getClient(this, googleSignInOptions)
 
         val tvNoAcc = findViewById<TextView>(R.id.tv_no_have_account)
         tvNoAcc.setOnClickListener{
@@ -19,8 +67,43 @@ class LoginUserActivity : AppCompatActivity() {
 
         val btnLogin = findViewById<Button>(R.id.btn_login)
         btnLogin.setOnClickListener{
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
+            email = edtEmail.text.toString().trim()
+            password = edtPassword.text.toString().trim()
+
+            if (email.isBlank() || password.isBlank()) {
+                Toast.makeText(this, "Vui lòng nhập đủ thông tin", Toast.LENGTH_SHORT).show()
+            } else {
+                signInUser(email, password)
+            }
+        }
+
+        val btnLoginGG = findViewById<Button>(R.id.btn_login_google)
+        btnLoginGG.setOnClickListener {
+            val signIntent = googleSignInclient.signInIntent
+            launcher.launch(signIntent)
+        }
+
+        val btnLoginFB = findViewById<Button>(R.id.btn_login_facebook)
+        btnLoginFB.setOnClickListener {
+            callbackManager = CallbackManager.Factory.create()
+            LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("public_profile", "email"))
+            LoginManager.getInstance().registerCallback(callbackManager,
+                object : FacebookCallback<LoginResult> {
+                    override fun onSuccess(result: LoginResult) {
+                        Log.d("LoginUser", "Facebook" + result.accessToken.token)
+                        val intent = Intent(this@LoginUserActivity, MainActivity::class.java)
+                        startActivity(intent)
+                    }
+
+                    override fun onCancel() {
+                        TODO("Not yet implemented")
+                    }
+
+                    override fun onError(error: FacebookException) {
+                        TODO("Not yet implemented")
+                    }
+
+                })
         }
 
         val tvForgotPassword = findViewById<TextView>(R.id.tv_forgot_password)
@@ -29,4 +112,67 @@ class LoginUserActivity : AppCompatActivity() {
             startActivity(intent)
         }
     }
+
+    private fun setUp() {
+        edtEmail = findViewById(R.id.edt_email_login)
+        edtPassword = findViewById(R.id.edt_password_login)
+
+    }
+
+    private fun signInUser(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener {task ->
+            if (task.isSuccessful) {
+                val user = auth.currentUser
+                updateUser(user)
+            } else {
+                Toast.makeText(this, "Tài khoản hoặc mật khẩu không đúng", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        callbackManager?.onActivityResult(resultCode, resultCode, data)
+    }
+
+
+    private fun updateUser(user: FirebaseUser?) {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    private val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+            handleResult(task)
+
+        }
+    }
+
+    private fun handleResult(task: Task<GoogleSignInAccount>) {
+        if (task.isSuccessful) {
+            val account : GoogleSignInAccount? = task.result
+            if (account != null) {
+                updateUI(account)
+            }
+        }else{
+            Toast.makeText(this, task.exception.toString(), Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun updateUI(account: GoogleSignInAccount) {
+        val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+        auth.signInWithCredential(credential).addOnCompleteListener{
+            if(it.isSuccessful) {
+                val intent : Intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }else{
+                Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+
 }
--- app/src/main/java/com/example/shopfoodptit/SignUpActivity.kt
+++ app/src/main/java/com/example/shopfoodptit/SignUpActivity.kt
@@ -1,11 +1,81 @@
 package com.example.shopfoodptit
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract.RawContacts.Data
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.example.shopfoodptit.model.UserModel
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.database
 
 class SignUpActivity : AppCompatActivity() {
+
+    private lateinit var auth : FirebaseAuth
+    private lateinit var userName : String
+    private lateinit var email : String
+    private lateinit var password : String
+    private lateinit var database : DatabaseReference
+    private lateinit var edtName: EditText
+    private lateinit var edtPassword: EditText
+    private lateinit var edtEmail: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+        khoiTao()
+
+        auth = Firebase.auth
+        database = Firebase.database.reference
+
+        val btnRegister = findViewById<Button>(R.id.btn_sign_up)
+
+        btnRegister.setOnClickListener{
+            email = edtEmail.text.toString().trim()
+            userName = edtName.text.toString().trim()
+            password = edtPassword.text.toString().trim()
+
+            if (email.isBlank() || userName.isBlank() || password.isBlank()){
+                Toast.makeText(this, "Vui lòng không để trống thông tin", Toast.LENGTH_SHORT).show()
+            }else{
+                registerUser(email, password)
+            }
+        }
+    }
+
+    private fun khoiTao() {
+        edtName = findViewById(R.id.edt_name)
+        edtPassword = findViewById(R.id.edt_password)
+        edtEmail = findViewById(R.id.edt_email)
+    }
+
+    private fun registerUser(email: String, password : String){
+        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener{task ->
+            if (task.isSuccessful) {
+                Toast.makeText(this, "Đã đăng ký tài khoản thành công", Toast.LENGTH_SHORT).show()
+                saveUser()
+                val intent = Intent(this, LoginUserActivity ::class.java)
+                startActivity(intent)
+                finish()
+            }else{
+                Toast.makeText(this, "Đã xảy ra lỗi khi đăng ký", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun saveUser() {
+        email = edtEmail.text.toString().trim()
+        userName = edtName.text.toString().trim()
+        password = edtPassword.text.toString().trim()
+        val user = UserModel(userName, email, password)
+        // Lưu thông tin khách hàng vào database của Firebase realtime
+        val userId = FirebaseAuth.getInstance().currentUser!!.uid
+        database.child("User").child(userId).setValue(user)
     }
 }
